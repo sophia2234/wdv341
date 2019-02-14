3b49ec97d059eca7c2abc039d5d9b5d8c8a26757
sophia2234 <47340695+sophia2234@users.noreply.github.com>
2019-02-14 10:46:50 -0600

    Add files via upload

--- testgit.php
+++ testgit.php
@@ -1,6 +1,5 @@
 <?php
-
-
+echo "Sophia Brucker";
 ?>
 <!doctype html>
 <html>
@@ -10,5 +9,24 @@
 </head>
 
 <body>
+	<h3>Git Related Terms:</h3>
+	<ul>
+	<li>Version Control Software</li>
+		<p>A software that records changes to a file or set of files over time so that you can recall specific versions later on.</p>
+		<li>Add</li>
+		<p>Adds all of the files in the current directory.</p>
+		<li>Commit</li>
+		<p>Used to save any changes to the local repository.</p>
+		<li>Push</li>
+		<p>Pushes commits made on a local branch to a remote repository.</p>
+		<li>Pull</li>
+		<p>Used to fetch and download content from a remote repository to match that content.</p>
+		<li>Clone</li>
+		<p>Used to target an existing repository and create a clone or copy of that repository.</p>
+	
+	
+	
+	
+	</ul>
 </body>
 </html>
